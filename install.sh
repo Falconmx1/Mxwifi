21453ce03e936a5f7e61df984c7c9786c1f13796
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-07-02 01:42:58 -0600

    Add installation script for Mxwifi

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Instalando Mxwifi..."
+sudo cp mxwifi.sh /usr/local/bin/mxwifi
+sudo chmod +x /usr/local/bin/mxwifi
+echo "Instalación completa. Ejecuta 'sudo mxwifi' para usar la herramienta."
